bb02dc376b7163c001fbb72ff4020b12b98286ad
Nolan Prochnau <parvus.mortalis@gmail.com>
2020-06-02 15:51:03 -0400

    Add material styles

--- todo-frontend/src/app/app.module.ts
+++ todo-frontend/src/app/app.module.ts
@@ -2,6 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +32,13 @@ import { CreateTodoComponent } from './create-todo/create-todo.component';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [TodoService],
   bootstrap: [AppComponent],
